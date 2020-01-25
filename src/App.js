1eb3bfa27ada237c3631433b276265e2c678c489
Boris Lukanovic <boris.lukanovic17@gmail.com>
2020-01-25 22:54:56 +0100

    start recipe app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
-function App() {
+const App = () => {
+  const APP_ID = "9af9f94c"
+  const APP_KEY = "0c9148e2c218837a35f10b0bf4c902a5	"
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  useEffect(() => {
+    console.log("useEffect in action")
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React!</h1>
+      <form className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
